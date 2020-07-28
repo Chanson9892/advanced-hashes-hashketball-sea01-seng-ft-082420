91b0ed05b139b9a01842bff0f12cfdb37ccfbe9f
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-28 01:27:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -160,3 +160,18 @@ def team_names
     team_data[:team_name]
   end
 end
+
+def player_numbers(team_name)
+  numbers_array = []
+  game_hash.each do |team, team_data|
+    if team_data[:team_name] == team_name
+      team_data[:players].each do |player|
+        player.each do |number|
+          if number == :number
+            numbers_array << number
+          end
+        end
+      end
+    end
+  end
+end
