2f6d58ce6994060c1b6993035c6c10795fa74e9a
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-28 00:44:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,3 +146,13 @@ def shoe_size(player_name)
     end
   end
 end
+
+def team_colors(team_name)
+  game_hash.each do |team, team_data|
+    team_data[:team_name].each do |team|
+      if team[:team_name] == team_name
+        return team[:colors]
+      end
+    end
+  end
+end
